0ce6281c867b7281716008d5dd6791b92284b7cf
Estelle Chevallier <chevallier.estelle@gmail.com>
2021-03-31 17:52:31 +0200

    ids

--- routes/skills.js
+++ routes/skills.js
@@ -5,21 +5,25 @@ router.get("/", function (req, res, next) {
   res.json({
     skills: [
       {
+        id: 0,
         name: "Angular",
         cat: "FE",
         level: 8,
       },
       {
+        id: 1,
         name: "SCSS",
         cat: "FE",
         level: 9,
       },
       {
+        id: 2,
         name: "MongoDB",
         cat: "BE",
         level: 3,
       },
       {
+        id: 3,
         name: "express",
         cat: "BE",
         level: 4,
