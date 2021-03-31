e020a2223fa898767282d6846642a08fd147e74e
Estelle Chevallier <chevallier.estelle@gmail.com>
2021-03-31 17:39:27 +0200

    env

--- app.js
+++ app.js
@@ -3,13 +3,14 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
-
+const cors = require("cors");
 const indexRouter = require("./routes/index");
 const skillsRouter = require("./routes/skills");
 
 const app = express();
 
 app.use(logger("dev"));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
